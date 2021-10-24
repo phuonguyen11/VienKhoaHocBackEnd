52e072860a2e8aa16d7053034973b3b93de77782
galaway <84115190+galaway@users.noreply.github.com>
2021-10-24 10:47:11 +0700

    change-db-postgress

--- my-project/config/database.js
+++ my-project/config/database.js
@@ -4,12 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5000),
+        database: env('DATABASE_NAME', 'postgres'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', '123'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        ssl: env('DATABASE_SSL', false)
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
